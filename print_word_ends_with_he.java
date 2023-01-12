e0b345e6380d941fc8ea10d7f7ec355e398be9ee
Kanishka D <106093506+Kanishka-D@users.noreply.github.com>
2023-01-12 17:47:50 +0530

    Create print_word_ends_with_he.java

---
+++ print_word_ends_with_he.java
@@ -0,0 +1,25 @@
+public class YourClassNameHere {
+    public static void main(String[] args) {
+       String s="In the box";
+      s+=" ";
+       int n=s.length();
+       String w="";
+       for(int i=0;i<n;i++){
+         char ch=s.charAt(i);
+         if(ch!=' '){
+           w+=ch;
+         }
+          else{
+            //if(w.charAt(0)=='b') or
+              if(w.endsWith("he"))
+               System.out.println(w);
+            w="";
+          }
+         }
+       }
+       
+       
+    }
+    
+    OUTPUT:
+    the
