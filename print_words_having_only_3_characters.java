611379947956509827871703a357dfd8f327bf7d
Kanishka D <106093506+Kanishka-D@users.noreply.github.com>
2023-01-12 17:51:44 +0530

    Create print_words_having_only_3_characters.java

---
+++ print_words_having_only_3_characters.java
@@ -0,0 +1,26 @@
+public class YourClassNameHere {
+    public static void main(String[] args) {
+       String s="In the box";
+      s+=" ";
+       int n=s.length();
+       String w="";
+       for(int i=0;i<n;i++){
+         char ch=s.charAt(i);
+         if(ch!=' '){
+           w+=ch;
+         }
+          else{
+            //if(w.charAt(0)=='b') or
+              if(w.length()==3)
+               System.out.println(w);
+            w="";
+          }
+         }
+       }
+       
+       
+    }
+    
+    OUTPUT:
+    the
+    box
